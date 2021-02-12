4589b616d67ff26e03257186a00447e2cb9c7f8a
OzaronHack <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 23:47:38 +0400

    Create batonet.cpp

---
+++ batonet.cpp
@@ -0,0 +1,33 @@
+#class OZARON(); {APIENTRY bayanat->hack[OZARON]}
+
+typedef int strcpy(class) 
+
+Ozaron : Batonet->Hack {
+	class()
+
+	public: private:?? (public(private(PUBLIC)))
+	 	->>????
+	 	int public()=>{ ??OZARON->SIGNATURA("BAYANAT")->HACK() }
+
+	 Assembler: C++()
+
+	 Deconverter{
+	 	/*functio*/int mail() {
+	 		main() return Ozaror
+	 	}
+	 }
+
+	 int ANTRYPOINT( OZARON)+>
+	 ENTRYPOINT?? ** OZARON->INVENTORY<-BAYONAT[HACK?]->HACK( BATONET )
+
+	 BATONET->EXPLOIT()->BACKDOOR( BAYANAT->SIGNATURA )
+
+	 OZARON::exit()
+}
+
+SERIALIZE{ 
+OZARON}
+
+#return include 
+
+/* BATONET HACK
